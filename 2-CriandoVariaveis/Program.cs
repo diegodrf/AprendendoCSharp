40fb7ee57ceb6986659054ca1db8fe4856688d65
Diego Faria <diego.rdfaria@gmail.com>
2018-12-23 19:15:19 -0200

    Primero commit

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola novo teste");
+
+            int idade1;
+            idade1 = 37;
+
+            int idade2 = 29;
+
+            int soma;
+            soma = idade1 + idade2;
+
+            Console.WriteLine("Idade1 = " + idade1 + "\nIdade2 = " + idade2 + "\nsoma da duas idades = " + soma);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 3-TestaPontoFlutuante/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_TestaPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 1250.70;
+
+            Console.WriteLine("Meu salário é " + salario);
+
+            // Teste de divisão
+            double divisao = 5.0 / 2;
+
+            Console.WriteLine(divisao);
+            // Fim do teste
+
+            Console.ReadLine();
+        }
+    }
+}
