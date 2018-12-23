bf5e6c803a2b30462d320b9321621f4f6d4d28a6
Diego Faria <diego.rdfaria@gmail.com>
2018-12-23 20:13:08 -0200

    Adicionado projeto de strings e caracteres

---
+++ 5-TestaCaracteres/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _5_TestaCaracteres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letra = 'a';
+            Console.WriteLine(letra);
+
+            // 65 é a posiçaõ de "A" na tabela ASCII
+            char valor = (char)65;
+            Console.WriteLine(valor);
+
+            // Casting para converter 66 em char
+            valor = (char)(valor + 1);
+            Console.WriteLine(valor);
+
+            string palavra = "Alura Cursos Online de Tecnologia ";
+            Console.WriteLine(palavra);
+
+            //  2020 é convertido para string automaticamente e concatenado
+            palavra = palavra + 2020;
+            Console.WriteLine(palavra);
+        }
+    }
+}
